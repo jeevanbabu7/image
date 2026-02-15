ea462312fe9ce7c39e21a38601551c3763cb041b
jeevanbabu7 <jeevanbabu2710@gmail.com>
2026-02-16 01:33:07 +0530

    cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -11,10 +11,13 @@ const app = express();
 ensureUploadsDir();
 
 app.use(
-	cors({
-		origin: "*"
-	})
+  cors({
+    origin: "*",
+    methods: "*",
+    allowedHeaders: "*"
+  })
 );
+
 app.use(express.json({ limit: "1mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiLimiter, routes);
